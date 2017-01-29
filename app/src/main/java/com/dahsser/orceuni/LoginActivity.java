8e92ecce611ff4627b7af737f1f241f305b1c039
jfuertes <jfuertesl2@gmail.com>
2017-01-29 14:23:19 -0500

    ahora si

--- app/src/main/java/com/dahsser/orceuni/LoginActivity.java
+++ app/src/main/java/com/dahsser/orceuni/LoginActivity.java
@@ -3,6 +3,7 @@ package com.dahsser.orceuni;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,6 +16,11 @@ import com.android.volley.toolbox.Volley;
 
 import org.w3c.dom.Text;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
@@ -24,29 +30,59 @@ public class LoginActivity extends AppCompatActivity {
     }
     public void LoginOrce(View view){
         TextView cod = (TextView)findViewById(R.id.codigo);
-        String codigo = cod.getText().toString();
+        final String codigo = cod.getText().toString();
         TextView pass = (TextView)findViewById(R.id.password);
-        String password = pass.getText().toString();
-        final TextView mTextView = (TextView) findViewById(R.id.Log);
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://www.google.com";
+        final String password = pass.getText().toString();
 
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+        final TextView mTextView = (TextView) findViewById(R.id.Log);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://www.orce.uni.edu.pe/mobile/api/login.json.php";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        mTextView.setText("Response is: "+ response.substring(0,500));
+                        // response
+                        Log.d("Response", response);
+                        mTextView.setText("Response is: "+ response);
                     }
-                }, new Response.ErrorListener() {
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error.getMessage());
+                    }
+                }
+        ) {
             @Override
-            public void onErrorResponse(VolleyError error) {
-                mTextView.setText("That didn't work!");
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("cod", codigo);
+                params.put("psw", md5(password));
+                params.put("sesion", "1");
+
+                return params;
             }
-        });
-    // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        };
+        queue.add(postRequest);
+    }
+    public String md5(String s) {
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            for (int i=0; i<messageDigest.length; i++)
+                hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
